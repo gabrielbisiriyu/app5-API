21e954c7cef178a046e438a48cfa620b7b730fb8
Gabriel_Bisiriyu <guyex1996@gmail.com>
2023-08-08 09:50:23 -0700

    Return Temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template
+import pandas as pd 
+
 
 app=Flask(__name__) 
 
@@ -9,10 +11,16 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    temperature=23
+    station=station.zfill(6) 
+    data="data/TG_STAID"+station+".txt" 
+    df=pd.read_csv(data,sep=',',skiprows=20,parse_dates=["    DATE"]) 
+    df['TG0']=df['   TG'].mask(df['   TG']==-9999) /10 
+    temperature=df.loc[df['    DATE']==date]['TG0'].squeeze()
     return {"station":station,
             "date":date,
             "temperature":temperature} 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
+
+
